949fcdba8d159f2f55dd2750744b9646fc33542a
betapcode17 <quocdat19991712@gmail.com>
2025-07-19 17:00:34 +0700

    Move cinebot into FrontEnd folder

---
+++ FrontEnd/cinebot/.history/src/App_20250719155733.jsx
@@ -0,0 +1,13 @@
+import "./index.css";
+import Header from "./components/Header.jsx";
+function App() {
+  return (
+    <>
+      <div>
+        <Header />
+      </div>
+    </>
+  );
+}
+
+export default App;
---
+++ FrontEnd/cinebot/.history/src/components/Banner_20250719164445.jsx
@@ -0,0 +1,5 @@
+import React from "react";
+
+export const Banner = () => {
+  return <div className="w-[200px] h-[700px]  bg-cover bg-center">Banner</div>;
+};
